1053b7a64bf7e849a60aff35cc0b7d5d842929c8
lenyjambois29-ops <leny.jambois29@gmail.com>
2025-11-07 15:55:00 +0000

    feat: Ajout de script.js et première interaction DOM

---
+++ script.js
@@ -0,0 +1,7 @@
+// Cible par ID (le plus précis)
+const titrePrincipal = document.querySelector('#titre-principal');
+titrePrincipal.addEventListener('click', () => {
+  // Ce code ne s'exécute QUE si l'utilisateur clique.
+  console.log('Bouton cliqué !');
+});
+
